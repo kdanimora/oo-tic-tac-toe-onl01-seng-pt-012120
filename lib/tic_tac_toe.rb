430f9175ed9df50fae1f539ce1881a89fb7465dc
Karen Daniela <kdaniimora@gmail.com>
2020-02-16 00:43:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,7 +58,73 @@ end
    #- that is not taken 
  end 
  
+   def current_player 
+    if turn_count % 2 == 0  
+      "X"
+    else 
+      "O"
+    end
+  end 
+  
+  def turn 
+    puts "Pick a position from 1 to 9: "
+    user_i = gets.chomp 
+    user_i = input_to_index(user_i)
+    if valid_move?(user_i)
+      move(user_i, current_player)
+      display_board
+    else 
+      turn 
+    end
  
+  end 
+  
+  def won? 
+  WIN_COMBINATIONS.each do |combo|
+    if ((@board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X") ||
+    (@board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"))
+      return combo
+      end
+  end 
+  return nil
+  end 
+  
+  def full?
+    !@board.any? {|e| e == " "}
+  end 
+  
+  def draw?
+    if !won? && full?
+      true 
+    else 
+      false
+    end 
+  end 
+  
+  def over? 
+    if draw? || won?
+      true
+    elsif full?
+      false 
+    end 
+  end 
+  
+  def winner 
+   won? ? @board[won?[0]] : nil
+      
+  end
+  
+  def play 
+    turn until over?
+    if won?
+      puts "Congratulations #{winner}!"
+      elsif draw?
+      puts "mmm!"
+    end
+  end 
+  
+  
+end 
  
  end 
  
